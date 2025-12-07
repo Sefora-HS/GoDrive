2eadbd5344a54c0452fb391620771c2f88b546cb
Sefora-HS <sefora.sylla@hotmail.com>
2025-12-07 19:39:51 +0100

    templates footer (en cours)

---
+++ templates/footer.php
@@ -0,0 +1,32 @@
+<footer class="footer">
+        <img src="../assets/images/logo.png" alt="logo voiture" class="footer-logo">
+        <div class="coordonnees-infos">
+            <h2 class="footer-title">Coordonnées</h2>
+            <p>123 Maple Street<br>
+                Suite 45<br>
+                Springfield, IL 62704<br>
+                USA
+            </p>
+            <p>+1 (555) 123‑9876</p>
+            <p>support@godrive-example.com</p>
+        </div>
+
+        <div class="footer-nav">
+            <h2 class="footer-title">Navigation</h2>
+            <ul class="footer-nav">
+                <li class="footer-nav-item"><a href="#" class="footer-nav-link">Accueil</a></li>
+                <li class="footer-nav-item"><a href="#" class="footer-nav-link">Catalogue</a></li>
+                <li class="footer-nav-item"><a href="#" class="footer-nav-link">Reservation rapide</a></li>
+                <li class="footer-nav-item"><a href="#" class="footer-nav-link">FAQ</a></li>
+                <li class="footer-nav-item"><a href="#" class="footer-nav-link">Contact</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-form">
+            <h2 class="footer-title">Newsletter</h2>
+            <form action="newsletter.php" method="post" class="newsletter-form">
+                <input type="email" name="email" placeholder="Votre email" required>
+                <button type="submit">S'inscrire</button>
+            </form>
+        </div>
+</footer>
--- templates/header.php
+++ templates/header.php
@@ -2,19 +2,6 @@
     //recuperation du nom de la page actuel
     $current_page = basename($_SERVER['PHP_SELF']);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Koulen&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <title>GoDrive</title>
-</head>
-<body>
     <header class="header">
         <img src="../assets/images/logo.png" alt="logo voiture" class="logo">
         <nav class="navigation">
@@ -31,5 +18,3 @@
             <a href="#" class="nav-btn nav-btn-2">S'inscrire</a>
         </div>
     </header>
-</body>
-</html>
