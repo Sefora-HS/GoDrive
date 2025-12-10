0812076c98558375b7af16e93e208fd40dadf24e
Sefora-HS <sefora.sylla@hotmail.com>
2025-12-10 13:39:58 +0100

    index

--- index.php
+++ index.php
@@ -15,7 +15,40 @@
 include('templates/header.php');
 ?>
 <main>
-    
+     <section class="section-index">
+        <!--  -->
+        <div class="hero-top">
+            <!--  -->
+            <div class="hero-left">
+                <h1 class="hero-title">Vous avez besoin d'une voiture ?</h1>
+                <h2 class="hero-subtitle">Reservez des maintenant votre voiture et profitez de la route</h2>
+
+                <div class="buttons-hero">
+                    <a href="#">Je reserve !</a>
+                    <a href="#" class="clair">En savoir plus</a>
+                </div>
+            </div>
+            <!--  -->
+            <div class="hero-right">
+                <img src="./assets/images/car-3d.png" alt="img voiture">
+            </div>
+        </div>
+
+        
+        <div class="hero-logos">
+            <div class="logo-container">
+                <img src="./assets/images/vw.png" alt="vw">
+                <img src="./assets/images/audi.png" alt="audi">
+                <img src="./assets/images/toyota.png" alt="toyota">
+                <img src="./assets/images/renault.png" alt="renault">
+                <img src="./assets/images/opel.png" alt="opel">
+                <img src="./assets/images/nissan.png" alt="nissan">
+                <img src="./assets/images/mercedes.png" alt="mercedes">
+                <img src="./assets/images/fiat.png" alt="fiat">
+                <img src="./assets/images/citroen.png" alt="citroen">
+            </div>
+        </div>
+     </section>
 </main>
 <?php
 // Inclut le header
