3524139c3430ea4078544b430fd5c1b8fe7fdcd2
Sefora-HS <sefora.sylla@hotmail.com>
2025-12-16 13:52:56 +0100

    base a jour

--- sql/concessionnaire.sql
+++ sql/concessionnaire.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 13, 2025 at 02:44 PM
+-- Generation Time: Dec 16, 2025 at 12:51 PM
 -- Server version: 8.4.7
 -- PHP Version: 8.3.28
 
@@ -35,7 +35,15 @@ CREATE TABLE IF NOT EXISTS `contact` (
   `message` text CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `nom`, `email`, `message`) VALUES
+(1, 'sefora', 'sefora@iuo.fr', 'testde msg'),
+(2, 'ji', 'ji@outlook.fr', 'ceci est un test effectué le 14 / 12 / 2025');
 
 -- --------------------------------------------------------
 
@@ -49,7 +57,14 @@ CREATE TABLE IF NOT EXISTS `newsletter` (
   `email` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Dumping data for table `newsletter`
+--
+
+INSERT INTO `newsletter` (`id`, `email`) VALUES
+(1, 'sor@65.ouf');
 
 -- --------------------------------------------------------
 
@@ -69,7 +84,17 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   PRIMARY KEY (`id`),
   KEY `id_utilisateur` (`id_utilisateur`),
   KEY `id_vehicule` (`id_vehicule`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`id`, `id_utilisateur`, `id_vehicule`, `date_debut`, `date_fin`, `total`, `message`) VALUES
+(1, 2, 2, '2025-12-19', '2025-12-28', 648.00, ''),
+(2, 2, 10, '2025-12-14', '2025-12-21', 2100.00, ''),
+(3, 2, 22, '2025-12-21', '2025-12-28', 1050.00, ''),
+(4, 2, 1, '2025-12-21', '2025-12-23', 100.00, '');
 
 -- --------------------------------------------------------
 
@@ -94,7 +119,16 @@ CREATE TABLE IF NOT EXISTS `reservation_rapide` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   KEY `fk_id_vehicule_3` (`id_vehicule`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Dumping data for table `reservation_rapide`
+--
+
+INSERT INTO `reservation_rapide` (`id`, `nom`, `prenom`, `email`, `adresse`, `ville`, `code_postal`, `id_vehicule`, `date_debut`, `date_fin`, `total`, `message`) VALUES
+(6, 'ff', 'fdd', 'ff.fdd@hj.fr', 'd,dovd', 'dggsgf', 98754, 2, '2025-12-17', '2025-12-27', 720, 'gsdgxc'),
+(7, 's', 'sy', 'test@testr.fr', '4 reu fer', 'ormo', 45000, 24, '2025-12-20', '2025-12-26', 300, 'dvsgdfhhndgn'),
+(8, 's', 's', 's@uy.gt', '12 rue des lui', 'ormoy', 91540, 7, '2025-12-22', '2025-12-29', 3150, 'ce\'est un test');
 
 -- --------------------------------------------------------
 
@@ -117,14 +151,15 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `identifiant` (`identifiant`),
   UNIQUE KEY `email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Dumping data for table `utilisateurs`
 --
 
 INSERT INTO `utilisateurs` (`id`, `identifiant`, `motdepasse`, `nom`, `prenom`, `email`, `adresse`, `ville`, `code_postal`, `role`) VALUES
-(1, 'Concessionnaire', 'Bv9x9h9U', '', '', '', '', '', '', 'client');
+(1, 'Concessionnaire', '$2y$12$1sSure6YtTHXuZNoSTwwn.lUPHJkQK/C724L.iK2JrKzYLXUXDf/e', '', '', '', '', '', '', 'admin'),
+(2, 's', '$2y$12$yYukZvZ2GzSN8zmbQikCtumbOE05.MbleHA7GHjKUcutIkmPx3hQu', 's', 's', 's@s.fr', '', '', '', 'client');
 
 -- --------------------------------------------------------
 
@@ -143,7 +178,7 @@ CREATE TABLE IF NOT EXISTS `vehicules` (
   `prix_jour` decimal(10,0) NOT NULL,
   `nb_places` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Dumping data for table `vehicules`
@@ -151,7 +186,36 @@ CREATE TABLE IF NOT EXISTS `vehicules` (
 
 INSERT INTO `vehicules` (`id`, `nom_vehicule`, `marque`, `annee_vehicule`, `description`, `image`, `prix_jour`, `nb_places`) VALUES
 (1, 'Peugeot 206', 'Peugeot', '1998', '', '206.jpg', 50, 5),
-(2, 'Nissan Qashqai', 'Nissan', '2007', '', '', 0, 0);
+(2, 'Nissan Qashqai', 'Nissan', '2007', '', 'qashqai.jpg', 72, 5),
+(3, 'Smart fortwo coupe', 'Smart', '2018', '', '693ee8db458e6.png', 25, 2),
+(4, 'Audi A3', 'Audi', '2020', '', '693eeeeaa2a8e.jpg', 250, 5),
+(5, 'BMW X5', 'BMW', '2019', '', '693eef1ec4fc3.jpg', 200, 5),
+(6, 'Clio 4', 'Renault', '2013', '', '693ef0a049764.jpg', 85, 3),
+(7, 'Audi SQ7', 'Audi', '2018', '', '693ef185be430.jpeg', 450, 5),
+(8, 'Audi RS6 Avant', 'Audi', '2020', '', '693ef1c746a43.jpg', 600, 5),
+(9, 'Dodge RAM', 'Dodge', '2022', '', '693ef22d94e27.jpeg', 400, 4),
+(10, 'Dodge charger SRT', 'Dodge', '1978', '', '693ef2ea9cdcf.jpg', 300, 4),
+(11, 'Lamborghini Aventador', 'Lamborghini', '2018', '', '693ef3792146f.jpg', 600, 2),
+(12, 'VW T-ROC', 'VW ', '2021', '', '693f0e695e438.jpg', 150, 5),
+(13, 'Toyota yaris', 'Toyota', '2023', '', '693f23cde7da4.jpg', 37, 5),
+(14, 'BMW M3 CS', 'BMW', '2023', '', '693f2698225f3.jpg', 250, 5),
+(15, 'AUDI RS3', 'AUDI', '2022', '', '693f26d911a8c.jpg', 250, 4),
+(16, 'Peugeot 208', 'Peugeot', '2024', '', '693f276cefd50.jpeg', 150, 5),
+(17, 'Porsche Cayenne', 'Porsche', '2024', '', '693f284d35e2f.jpg', 300, 5),
+(18, 'Lamborghini Urus', 'Lamborghini', '2022', '', '693f28a0d2a63.jpg', 600, 5),
+(19, 'Peugeot 3008', 'Peugeot', '2022', '', '693f291d7c623.jpg', 150, 5),
+(20, 'Tesla Model S', 'Tesla', '2018', '', '693f29a12f736.jpg', 200, 4),
+(21, 'Range Rover sport', 'Range Rover', '2023', '', '693f2a14f1fa2.jpg', 420, 5),
+(22, 'Dodge challenger', 'Dodge', '1971', '', '693f2afa1a8f6.jpg', 150, 4),
+(23, 'VW Golf 7', 'VW', '2020', '', '693f2b608fb4e.jpg', 75, 5),
+(24, 'Citroen ami', 'Citroen', '2025', '', '693f2bab29107.jpg', 50, 2),
+(25, 'Fiat 500', 'Fiat', '2024', '', '693f2be59c88b.jpg', 100, 2),
+(26, 'Suzuki swift', 'Suzuki', '2024', '', '693f2c409b7e1.jpg', 75, 4),
+(27, 'Mini Cooper', 'Mini', '2024', '', '693f2c82abe85.jpg', 130, 4),
+(28, 'Toyota aygo', 'Toyota', '2025', '', '693f2cd833714.jpg', 100, 4),
+(29, 'Citroen c3', 'Citroen', '2025', '', '693f2d25d969f.jpg', 90, 5),
+(30, 'Suzuki ignis', 'Suzuki', '2024', '', '693f2daa697ae.jpg', 50, 4),
+(31, 'Audi a5', 'audi', '2020', '', '694146b72bf57.jpg', 250, 5);
 
 --
 -- Constraints for dumped tables
